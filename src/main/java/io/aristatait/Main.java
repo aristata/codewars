c533f40c1dfffcae7f41be34d956beb9e1626132
장성민 <earnestgem@optatumplatform.com>
2022-06-09 09:14:33 +0900

    8 kyu: Convert a Number to a String!

---
+++ src/main/java/io/aristatait/Main.java
@@ -0,0 +1,11 @@
+package io.aristatait;
+
+/**
+ * 최초 작성자 castlepeople
+ * 최초 작성일 2022-06-09
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/io/aristatait/kyu8/ConvertNumToString.java
@@ -0,0 +1,15 @@
+package io.aristatait.kyu8;
+
+/**
+ * 최초 작성자 castlepeople
+ * 최초 작성일 2022-06-09
+ *
+ * Q) 숫자를 문자열로 변환할 수 있는 함수가 필요합니다. 이것을 달성하는 어떤 방법을 알고 있습니까?
+ * Ex)  123 --> "123"
+ *      999 --> "999"
+ **/
+public class ConvertNumToString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
